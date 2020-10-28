9dccc9f8120d8c63d737faf52d1aec8794e4f56b
Sagar Patil <sagarreddypatil@gmail.com>
2020-10-28 10:02:04 -0400

    Somethign

---
+++ main.py
@@ -0,0 +1,17 @@
+import gym
+import gym_chess
+import random
+
+env = gym.make("ChessVsRandomBot-v0")
+env.reset()
+
+done = False
+
+while not done:
+    actions = env.get_possible_actions(state, player)
+    action = random.choice(actions)
+    observation, reward, done, info = env.step(action)
+    print(env.render())
+    print(f"Reward: {reward}\n")
+
+env.close()
